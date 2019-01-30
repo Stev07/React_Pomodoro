4342ac4eac8469886e499628a9d0276d9b90ffc3
Steve Dossin <steve.dossin@gmail.com>
2019-01-30 16:19:55 +0100

    fix clearInterval + modal

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './Timer.css';
 import './Box.css';
+import Modal from 'react-modal';
 
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    width                 :'400px',
+    height                :'200px'
+  }
+};
+
+
+Modal.setAppElement('#app')
 
 class Timer extends React.Component {
     constructor(props) {
@@ -10,49 +27,66 @@ class Timer extends React.Component {
       this.state = {
         seconds: 5,
         timerID: null,
-        active:true
+        active:false,
+        showModal: false
       };
 
       this.addTime = this.addTime.bind(this);
       this.lessTime = this.lessTime.bind(this);
-      this.startStop = this.startStop.bind(this);
+      this.startReset = this.startReset.bind(this);
       this.start = this.start.bind(this);
-      this.stop = this.stop.bind(this);
+      this.reset = this.reset.bind(this);
+      this.handleOpenModal = this.handleOpenModal.bind(this);
+      this.handleCloseModal = this.handleCloseModal.bind(this);
+      this.restart = this.restart.bind(this);
 
     }
 
+    handleOpenModal () {
+      this.setState({ showModal: true });
+    }
+    
+    handleCloseModal () {
+      this.setState({ showModal: false });
+    }
 
+    restart(){
+      this.handleCloseModal()
+      this.start()
+    }
 
     start(){
-      document.querySelector('.start').innerHTML = "Stop"
-      this.setState({active:false})
-
-      if(this.state.active === false){
-        document.querySelector('.more').style.backgroundColor="black";
-      }
+      document.querySelector('.start').innerHTML = "Reset"
+      this.setState({active:true})
+      
 
       let count = this.setState({timerID: setInterval(()=>{
         if(this.state.seconds>0){
           this.setState({ seconds: this.state.seconds - 1})
         }else{
           clearInterval(count)
+          this.handleOpenModal()
+          clearInterval(this.state.timerID)
+          this.setState({seconds: this.state.seconds =5})
+          
+
         }
       },1000)})
-
     }
 
-    stop() {
+    reset() {
       clearInterval(this.state.timerID)
       this.setState({
         seconds:this.state.seconds = 5
       })
 
-      document.querySelector('.start').innerHTML = "Start"   
+      document.querySelector('.start').innerHTML = "Start"
+      this.setState({active:false})   
     }
 
-    startStop() {
+    startReset() {
       document.querySelector('.start').innerHTML == "Start" ? 
-        this.start() : this.stop()
+        this.start() : this.reset()
     }
 
 
@@ -89,16 +123,32 @@ class Timer extends React.Component {
 
       return `${h}:${m}:${s}`;
     }
+
+
+
+
   
     render() {
+
       return (
         <div className="box">
+        {/* <button onClick={this.handleOpenModal}>Trigger Modal</button> */}
+        <Modal
+          style={customStyles}
+          isOpen={this.state.showModal}
+          contentLabel="Minimal Modal Example"
+        > 
+          <p>Have a break ...</p>
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+          <button onClick={this.restart}>Restart</button>
+        
+        </Modal>
           <div className="time">
           {this.convertToHhMmSs(this.state.seconds)}
           </div>
-            <div className="more" onClick={this.addTime}>+</div>
-            <div className="start" onClick={this.startStop}>Start</div>
-            <div className="less"onClick={this.lessTime}>-</div>
+            <button className="more" disabled={this.state.active} onClick={this.addTime}>+</button>
+            <button className="start" onClick={this.startReset}>Start</button>
+            <button className="less" disabled={this.state.active} onClick={this.lessTime}>-</button>
         </div>
 
       );
